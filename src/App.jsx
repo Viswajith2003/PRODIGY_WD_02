2c80f1ab29d4b097c709284e3144ac91c25a51dc
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-13 17:22:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.js
+
+import "./App.css";
+import Stopwatch from "./Stopwatch";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
